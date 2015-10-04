1a4ffca92346587e3acc6b234ff9d0dc95eea5b3
gamazeps <gamaz3ps@gmail.com>
2015-10-05 01:14:01 +0400

    Very basic printer structure.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::VecDeque;
+
+struct PrinterActor {
+    callbacks: VecDeque<String>,
+}
+
+impl PrinterActor {
+    fn new() -> PrinterActor {
+        PrinterActor {
+            callbacks: VecDeque::new(),
+        }
+    }
+
+    fn send(&mut self, message: String) {
+        self.callbacks.push_back(message);
+    }
+
+    fn treat_messages(&mut self) {
+        if let Some(s) = self.callbacks.pop_front() {
+            println!("{}", s);
+        }
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut actor = PrinterActor::new();
+
+    actor.send("Hello world!".to_string());
+
+    actor.treat_messages();
 }
