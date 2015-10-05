04c598d43a51ff3037e3ba0f8c9b3d3f4cde55a8
gamazeps <gamaz3ps@gmail.com>
2015-10-06 02:51:56 +0400

    The Actors can  now be added to the main FIFO.
    This is done by adding a wrapper around the Actor struct.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,82 @@
-use std::cell::RefCell;
 use std::collections::VecDeque;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
-trait Actor {
-    fn treat_messages(&mut self);
-}
-
-struct PrinterActor {
+struct Actor {
     // VecDeque is the current recommendation for FIFOs.
-    callbacks: VecDeque<String>,
-    // TODO(gamazeps): Find the proper type for the main queue.
-    worker_queue: Arc<RefCell<VecDeque<PrinterActor>>>,
+    // Right now this is a simple FIFO for messages to be printed.
+    messages_queue: VecDeque<String>,
 }
 
-impl Actor for PrinterActor {
+impl Actor {
+    fn new(messages: VecDeque<String>) -> Actor {
+        Actor {
+           messages_queue: messages,
+        }
+    }
+
+    // Right now the send and treat_messages methods are not in the same structs, which is a bad
+    // API, this will be changed once the main features are working.
     fn treat_messages(&mut self) {
-        if let Some(s) = self.callbacks.pop_front() {
+        if let Some(s) = self.messages_queue.pop_front() {
             println!("{}", s);
         }
     }
 }
 
-impl PrinterActor {
-    fn new(queue: Arc<RefCell<VecDeque<PrinterActor>>>) -> PrinterActor {
-        PrinterActor {
-            callbacks: VecDeque::new(),
+// The ActorWrapper struct is a way to be able to add Actors to the FIFO while staying safe and
+// clean.
+struct ActorWrapper {
+    // Since the FIFO will be acccessed in a concurrent manner, wrapping it around an Arc and Mutex
+    // is needed.
+    // This FIFO contains Actors wrapped in the same way for the same reason.
+    worker_queue: Arc<Mutex<VecDeque<Arc<Mutex<Actor>>>>>,
+    // Having an inner actor is needed as it is extremely messy to have the actor add itself to the
+    // FIFO.
+    // The Actor is put in an Arc to avoid dangling pointers, and the Mutex is there for concurrent
+    // mutability.
+    actor: Arc<Mutex<Actor>>,
+}
+
+impl ActorWrapper {
+    fn new(
+        actor: Arc<Mutex<Actor>>,
+        queue: Arc<Mutex<VecDeque<Arc<Mutex<Actor>>>>>) -> ActorWrapper {
+        ActorWrapper {
             worker_queue: queue,
+            actor: actor,
         }
     }
 
     fn send(&mut self, message: String) {
-        self.callbacks.push_back(message);
+        {
+            let mut actor = self.actor.lock().unwrap();
+            actor.messages_queue.push_back(message);
+        }
+
+        let mut queue = self.worker_queue.lock().unwrap();
+        queue.push_back(self.actor.clone());
     }
 }
 
 fn main() {
-    // Arc allows managing a shared ressource in a thread safe way.
-    // RefCell allows sharing a mutable reference.
-    let shared_queue: Arc<RefCell<VecDeque<PrinterActor>>> = Arc::new(RefCell::new(VecDeque::new()));
-    let mut actor = PrinterActor::new(shared_queue.clone());
+    let shared_queue: Arc<Mutex<VecDeque<_>>> = Arc::new(Mutex::new(VecDeque::new()));
 
-    actor.send("Hello world!".to_string());
+    let mut actor_1 = ActorWrapper::new(
+        Arc::new(Mutex::new(Actor::new(VecDeque::new()))),
+        shared_queue.clone());
+    let mut actor_2 = ActorWrapper::new(
+        Arc::new(Mutex::new(Actor::new(VecDeque::new()))),
+        shared_queue.clone());
 
-    actor.treat_messages();
+    // The messages should be printed in this order.
+    actor_1.send("Hello world 1-1!".to_string());
+    actor_2.send("Hello world 2!".to_string());
+    actor_1.send("Hello world 1-2!".to_string());
+
+    loop {
+        let mut queue = shared_queue.lock().unwrap();
+        if let Some(actor) = queue.pop_front() {
+            actor.lock().unwrap().treat_messages();
+        }
+    }
 }
