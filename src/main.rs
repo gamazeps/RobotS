8efa8f8d8b167d00652a7f69b7f55d0343be4977
gamazeps <gamaz3ps@gmail.com>
2015-10-15 03:10:27 +0400

    WIP - maybe bug on rustc

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,51 @@
-use std::collections::VecDeque;
-use std::mem;
+extern crate core;
+
 use std::sync::{Arc, Mutex};
-use std::thread;
+use std::collections::{HashMap, VecDeque};
 
-enum Message {
-    Data(String),
-    Stop,
+enum Message<T> {
+    Command(String),
+    Data(T),
 }
 
-struct Actor {
-    // TODO(gamazeps): Need to add Arc<Mutex<>> for this to be Send.
-    // Use Unique instead (or maybe not...)
-    ptr: *mut ActorInner,
+struct ActorRef<T: Send + Sized> {
+    name: String,
+    message_queue: Arc<Mutex<VecDeque<Message<T>>>>,
 }
 
-impl Actor {
-    fn new(queue: Arc<Mutex<VecDeque<Actor>>>) -> Actor {
-        // Memory is allocated for an ActorInner.
-        let ptr = Box::new(ActorInner::new(queue));
-        // Ownership is passed to the created object, it will be deallocated when the object is
-        // dropped.
-        Actor {
-            ptr: unsafe {
-                mem::transmute(ptr)
-            }
+trait Actor {}
+
+impl<T: Send + Sized> ActorRef<T> {
+    fn new(name: String) -> ActorRef<T> {
+        ActorRef {
+            name: name,
+            message_queue: Arc::new(Mutex::new(VecDeque::new())),
+        }
+    }
+}
+
+struct ActorSystem {
+    actors_table: Arc<Mutex<HashMap<String, ActorRef<Send + core::marker::Sized>>>>,
+}
+
+impl ActorSystem {
+    fn new() -> ActorSystem {
+        ActorSystem {
+            actors_table: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
-    fn inner(&self) -> &ActorInner {
-        unsafe {&*self.ptr}
-    }
-
-    fn inner_mut(&mut self) -> &mut ActorInner {
-        unsafe {&mut *self.ptr}
-    }
-
-    fn transmute_inner_mut(&self) -> &mut ActorInner {
-        unsafe {&mut *self.ptr}
-    }
-
-    fn receive(&self, message: Message) {
-        self.transmute_inner_mut().receive(message);
-    }
-
-    fn treat(&self) {
-        self.transmute_inner_mut().treat();
-    }
-
-    fn treat_all(&self) {
-        self.transmute_inner_mut().treat_all();
-    }
-}
-
-unsafe impl Send for Actor {}
-
-// Note that this struct should not be instantied by hand.
-// TODO(gamazeps): create a module to have enforce this.
-// An InnerActor  is dropped when the Actor that created it (with new) is dropped.
-// This means that if the Actor goes out of scope while it still has a message to treat,
-// there will be a segfault (or at least UB).
-// This is a design choice for now (otherwise we just need to take inspiration from the code in Arc).
-struct ActorInner {
-    messages: VecDeque<Message>,
-    actor_queue: Arc<Mutex<VecDeque<Actor>>>,
-}
-
-impl ActorInner {
-    fn new(queue: Arc<Mutex<VecDeque<Actor>>>) -> ActorInner {
-        ActorInner {
-            messages: VecDeque::new(),
-            actor_queue: queue,
-        }
-    }
-
-    fn receive(&mut self, message: Message) {
-        self.messages.push_back(message);
+    fn create_actor_ref(&self, handle: String) {
+        let actor_ref: ActorRef<i32> = ActorRef::new(handle);
         {
-            let mut queue = self.actor_queue.lock().unwrap();
-            queue.push_back(self.actor());
+            let actors_table = self.actors_table.lock().unwrap();
+            actors_table.insert(handle, actor_ref);
         }
     }
 
-    // TODO(gamazeps): There might be a useless copy of data, deal with it later.
-    fn treat_message(&self, message: Message) {
-        match message {
-            Message::Data(s) => println!("{}", s),
-            Message::Stop => println!("Stop received"),
-        }
-    }
-
-
-    fn treat(&mut self) {
-        if let Some(message) = self.messages.pop_front() {
-            self.treat_message(message);
-        }
-    }
-
-    fn treat_all(&mut self) {
-        while let Some(message) = self.messages.pop_front() {
-            self.treat_message(message);
-        }
-    }
-
-    fn actor(&self) -> Actor {
-        Actor{
-            ptr: unsafe {
-                mem::transmute(self)
-            }
-        }
-    }
 }
-
-fn spawn_consumer(actor_queue: Arc<Mutex<VecDeque<Actor>>>) -> thread::JoinHandle<usize> {
-    thread::spawn(move || {
-        loop {
-            let actor;
-            {
-                let mut queue = actor_queue.lock().unwrap();
-                actor = queue.pop_front();
-            }
-            if let Some(actor) = actor {
-                actor.treat();
-            }
-        }
-    })
-}
-
 
 fn main() {
-    let actor_queue = Arc::new(Mutex::new(VecDeque::new()));
-    let actor_1 = Actor::new(actor_queue.clone());
-    let actor_2 = Actor::new(actor_queue.clone());
-
-    actor_1.receive(Message::Data("message 1".to_string()));
-    actor_2.receive(Message::Data("message 2".to_string()));
-    actor_1.receive(Message::Data("message 3".to_string()));
-    actor_2.receive(Message::Data("message 4".to_string()));
-
-    let handle = spawn_consumer(actor_queue.clone());
-
-    handle.join();
-
-    println!("Hello World");
+    let _actor: ActorRef<String> = ActorRef::new("actor_1".to_string());
+    println!("Hello world");
 }
