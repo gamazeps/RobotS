1e8a7d82a845eb94f737ce2e6538c820514b5f62
gamazeps <gamaz3ps@gmail.com>
2015-10-20 19:46:45 +0400

    Proper ActorRef, with trait ojects, also removed genericity from the Message using Any

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,103 @@
+use std::any::Any;
+use std::collections::{HashSet, VecDeque};
 use std::sync::{Arc, Mutex};
-use std::collections::{HashMap, VecDeque};
 
-enum Message<T> {
+enum Message {
     Command(String),
-    Data(T),
+    Data(Box<Any + Send>),
 }
 
-struct ActorRef<T: Send + Sized> {
-    name: Arc<String>,
-    message_queue: Arc<Mutex<VecDeque<Message<T>>>>,
+type ActorRef = Arc<Mutex<Actor>>;
+
+trait Actor {
+    fn receive(&mut self, Message);
+    fn handle_message(&mut self);
 }
 
-impl<T: Send + Sized> ActorRef<T> {
-    fn new(name: Arc<String>) -> ActorRef<T> {
-        ActorRef {
+struct Printer {
+    name: String,
+    message_queue: VecDeque<Message>,
+}
+
+impl Printer {
+    fn new(name: String) -> Printer {
+        Printer {
             name: name,
-            message_queue: Arc::new(Mutex::new(VecDeque::new())),
+            message_queue: VecDeque::new(),
         }
     }
+}
 
-    fn receive(&self, message: Message<T>) {
-        self.message_queue.lock().unwrap().push_back(message);
+impl Actor for Printer {
+    fn receive(&mut self, message: Message) {
+        self.message_queue.push_back(message);
     }
 
-    fn handle(&self) {
-        let message;
-        {
-            message = self.message_queue.lock().unwrap().pop_front();
-        }
-        let message = message.unwrap();
+    fn handle_message(&mut self) {
+        let message = self.message_queue.pop_front().unwrap();
+
         match message {
-            Message::Command(_) => println!("aya captain"),
-            Message::Data(_) => println!("interesting data that you have"),
+            Message::Command(command) => {
+                println!("Received Command: ({})", command)
+            },
+            Message::Data(truc) => {
+                match truc.downcast_ref::<String>() {
+                    Some(s) => println!("Received data: ({})", s),
+                    None => println!("Message is dropped"),
+                }
+            }
         }
     }
-
 }
 
-struct ActorSystem<T: Send + Sized> {
-    actors_table: Arc<Mutex<HashMap<Arc<String>, ActorRef<T>>>>,
+struct ActorSystem {
+    // TODO(gamazeps): Use an unordered container instead.
+    // There is currently an issue with having an ActorRef as a Arc<Mutex<Actor + Eq + Hash>>.
+    actors_table: Mutex<Vec<ActorRef>>,
+    actors_queue: Arc<Mutex<VecDeque<ActorRef>>>,
 }
 
-impl<T: Send + Sized> ActorSystem<T> {
-    fn new() -> ActorSystem<T> {
+
+impl ActorSystem {
+    fn new() -> ActorSystem {
         ActorSystem {
-            actors_table: Arc::new(Mutex::new(HashMap::new())),
+            actors_table: Mutex::new(Vec::new()),
+            actors_queue: Arc::new(Mutex::new(VecDeque::new())),
         }
     }
 
-    fn create_actor_ref(&self, handle: String) {
-        let handle = Arc::new(handle);
-        let actor_ref: ActorRef<T> = ActorRef::new(handle.clone());
+    fn spawn_actor(&self, name: String) -> ActorRef {
+        let actor_ref = Arc::new(Mutex::new(Printer::new(name)));
         {
             let mut actors_table = self.actors_table.lock().unwrap();
-            actors_table.insert(handle.clone(), actor_ref);
+            actors_table.push(actor_ref.clone());
         }
+        actor_ref
     }
 
-    fn send_message_to_actor(&self, dont_judge_me: Arc<String>, message: Message<T>) {
-        let actors_table = self.actors_table.lock().unwrap();
-        // Here this is very bad as get gives a ref, and thus we need to keep the lock for a long
-        // time (and we also need the actor's message queue lock).
-        let actor_ref = actors_table.get(&dont_judge_me.clone());
-        match actor_ref {
-            Some(actor_ref) => {
-                println!("yay");
-                actor_ref.receive(message);
-                actor_ref.handle();
-            }
-            None => println!("oh noes"),
+    fn send_to_actor(&self, actor_ref: ActorRef, message: Message) {
+        {
+            let mut actor = actor_ref.lock().unwrap();
+            actor.receive(message);
+        }
+        {
+            let mut actors_queue = self.actors_queue.lock().unwrap();
+            actors_queue.push_back(actor_ref);
         }
     }
 }
 
 fn main() {
-    let actor_system: ActorSystem<i32> = ActorSystem::new();
-    actor_system.create_actor_ref("actor1".to_string());
-    actor_system.create_actor_ref("actor2".to_string());
-    actor_system.send_message_to_actor(Arc::new("actor1".to_string()), Message::Data(1i32));
-    actor_system.send_message_to_actor(Arc::new("actor2".to_string()), Message::Data("ay ay"));
+    let message = "This is a message".to_string();
+    let command = "This is a command".to_string();
 
-    println!("Hello world");
+    let mut actor = Printer::new("actor_1".to_string());
+
+    actor.receive(Message::Command(command));
+    actor.receive(Message::Data(Box::new(message)));
+    actor.receive(Message::Data(Box::new(3i32)));
+
+    actor.handle_message();
+    actor.handle_message();
+    actor.handle_message();
 }
