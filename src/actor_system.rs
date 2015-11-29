17e5cf892a5d65be881aba8f98703bc0f0a2c778
gamazeps <gamaz3ps@gmail.com>
2015-11-29 13:59:34 +0100

    Use channel for enqueuing actors instead of a VecDeque.
    This will be usefull when we will work on the thread pools.

--- src/actor_system.rs
+++ src/actor_system.rs
@@ -1,7 +1,7 @@
 use std::any::Any;
-use std::collections::VecDeque;
+use std::process::exit;
 use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{channel, Sender, TryRecvError};
+use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
 use std::thread;
 
 use {Actor, ActorRef, CanReceive, Props};
@@ -20,8 +20,10 @@ pub struct ActorSystem {
     // For now we will have the worker pool in the system.
     // TODO(find a way to have a clean way to separate system and user threads).
     consumer_threads: Arc<Mutex<Vec<ConsumerThread>>>,
-    // TODO(gamazeps): Have a CanHandle Trait for that.
-    actors_queue: Arc<Mutex<VecDeque<Arc<CanReceive >>>>,
+    // Sends Canreceive to be handled on that channel.
+    actors_queue_sender: Arc<Mutex<Sender<Arc<CanReceive>>>>,
+    // Receiving end to give to the thread pool.
+    actors_queue_receiver: Arc<Mutex<Receiver<Arc<CanReceive>>>>,
 }
 
 impl ActorSystem {
@@ -29,10 +31,12 @@ impl ActorSystem {
     ///
     /// Note that no threads are started.
     pub fn new(name: String) -> ActorSystem {
+        let (tx, rx) = channel();
         ActorSystem {
             name: Arc::new(name),
             consumer_threads: Arc::new(Mutex::new(Vec::new())),
-            actors_queue: Arc::new(Mutex::new(VecDeque::new())),
+            actors_queue_sender: Arc::new(Mutex::new(tx)),
+            actors_queue_receiver: Arc::new(Mutex::new(rx)),
         }
     }
 
@@ -45,14 +49,20 @@ impl ActorSystem {
 
     /// Enqueues the given Actor on the queue of Actors with something to handle.
     pub fn enqueue_actor<Args: Copy + Send + Sync + 'static, M: Copy + Send + Sync + 'static + Any, A: Actor<M> + 'static>(&self, actor_ref: ActorRef<Args, M, A>) {
-        self.actors_queue.lock().unwrap().push_back(Arc::new(actor_ref));
+        match self.actors_queue_sender.lock().unwrap().send(Arc::new(actor_ref)) {
+            Ok(_) => return,
+            Err(_) => {
+                println!("The communication channel for messages is disconnected, this is bad!");
+                exit(1);
+            },
+        }
     }
 
     /// Spawns a thread that will consume messages from the `ActorRef` in `actors_queue`.
     /// This thread can be terminated by calling `terminate_thread`.
     pub fn spawn_thread(&self) {
         let (tx, rx) = channel();
-        let thread_system = self.clone();
+        let actors_queue = self.actors_queue_receiver.clone();
         let handle = thread::spawn(move || {
             loop {
                 // If we received a () we kill the thread.
@@ -63,10 +73,20 @@ impl ActorSystem {
                     },
                     Err(TryRecvError::Empty) => {}
                 };
-                // Else we try to prcess a message.
-                let actor_ref = {thread_system.actors_queue.lock().unwrap().pop_front()};
-                for actor in actor_ref.iter() {
-                    actor.handle();
+
+                // Else we try to process a message.
+                let actor_ref = {
+                    let lock = actors_queue.lock().unwrap();
+                    lock.try_recv()
+                };
+
+                match actor_ref {
+                    Ok(actor_ref) => actor_ref.handle(),
+                    Err(TryRecvError::Empty) => continue,
+                    Err(TryRecvError::Disconnected) => {
+                        println!("The actors queue failed, something is very wrong");
+                        exit(1);
+                    }
                 }
             }
         });
@@ -108,7 +128,8 @@ impl Clone for ActorSystem {
         ActorSystem {
             name: self.name.clone(),
             consumer_threads: self.consumer_threads.clone(),
-            actors_queue: self.actors_queue.clone(),
+            actors_queue_sender: self.actors_queue_sender.clone(),
+            actors_queue_receiver: self.actors_queue_receiver.clone(),
         }
     }
 }
