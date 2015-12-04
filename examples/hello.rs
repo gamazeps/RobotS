d7b346cc530f3175ce8c4a94d0d716b4681b4f90
gamazeps <gamaz3ps@gmail.com>
2015-12-04 19:02:58 +0100

    Add a hello world example

---
+++ examples/hello.rs
@@ -0,0 +1,62 @@
+extern crate robots;
+
+use std::sync::Arc;
+use std::time::Duration;
+
+use robots::actors::{Actor, ActorSystem, ActorCell, ActorContext, Props, Message};
+
+#[derive(Copy, Clone, PartialEq)]
+enum Greetings {
+    Greet,
+    Done,
+}
+
+struct HelloWorld;
+
+impl Actor<Greetings> for HelloWorld {
+    fn pre_start<Args: Message>(&self, context: ActorCell<Args, Greetings, HelloWorld>) {
+        let props = Props::new(Arc::new(Greeter::new), ());
+        let greeter = context.actor_of(props);
+        context.tell(greeter, Greetings::Greet);
+    }
+    fn receive<Args: Message>(&self, message: Greetings, _context: ActorCell<Args, Greetings, HelloWorld>) {
+        if message == Greetings::Done {
+            println!("The greeting is done");
+        }
+    }
+}
+
+impl HelloWorld {
+    fn new(_dummy: ()) -> HelloWorld {
+        HelloWorld
+    }
+}
+
+struct Greeter;
+
+impl Actor<Greetings> for Greeter {
+    fn receive<Args: Message>(&self, message: Greetings, context: ActorCell<Args, Greetings, Greeter>) {
+        if message == Greetings::Greet {
+            println!("Hello World");
+            context.tell(context.sender(), Greetings::Done);
+        }
+    }
+}
+
+impl Greeter {
+    fn new(_dummy: ()) -> Greeter {
+        Greeter
+    }
+}
+
+fn main() {
+    let actor_system = ActorSystem::new("test".to_owned());
+    actor_system.spawn_threads(1);
+
+    let props = Props::new(Arc::new(HelloWorld::new), ());
+    let _actor = actor_system.actor_of(props);
+
+    std::thread::sleep(Duration::from_millis(100));
+
+    actor_system.terminate_threads(1);
+}
