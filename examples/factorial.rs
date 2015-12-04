7cae907dcc9b4de2aa570529c21d32920196f2a1
gamazeps <gamaz3ps@gmail.com>
2015-12-04 20:32:57 +0100

    Remove the main and use examples instead.

---
+++ examples/factorial.rs
@@ -0,0 +1,42 @@
+extern crate robots;
+
+use std::sync::Arc;
+use std::time::Duration;
+
+use robots::actors::{Actor, ActorSystem, ActorCell, ActorContext, Props, Message};
+
+/// Basic factorial.
+struct Factorial;
+
+impl Actor<(u32, u32)> for Factorial {
+    fn receive<Args: Message>(&self, message: (u32, u32), context: ActorCell<Args, (u32, u32), Factorial>) {
+        let (i, j) = message;
+        if i == 0 {
+            println!("factorial: {}", j);
+        } else {
+            context.tell(context.actor_ref(), (i - 1, j * i));
+        }
+    }
+}
+
+impl Factorial {
+    fn new(_dummy: ()) -> Factorial {
+        Factorial
+    }
+}
+
+fn main() {
+    let actor_system = ActorSystem::new("test".to_owned());
+    actor_system.spawn_threads(2);
+
+    let props_factorial = Props::new(Arc::new(Factorial::new), ());
+    let factorial_actor_ref_1 = actor_system.actor_of(props_factorial.clone());
+    let factorial_actor_ref_2 = actor_system.actor_of(props_factorial.clone());
+
+    factorial_actor_ref_1.tell_to(factorial_actor_ref_2.clone(), (3u32, 1u32));
+    factorial_actor_ref_1.tell_to(factorial_actor_ref_2.clone(), (7u32, 1u32));
+    factorial_actor_ref_1.tell_to(factorial_actor_ref_2.clone(), (11u32, 1u32));
+
+    std::thread::sleep(Duration::from_millis(10));
+    actor_system.terminate_threads(2);
+}
---
+++ examples/panic.rs
@@ -0,0 +1,56 @@
+extern crate robots;
+
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+
+use robots::actors::{Actor, ActorSystem, ActorCell, ActorContext, Props, Message};
+
+#[derive(Copy, Clone)]
+enum InternalStateMessage {
+    Set(u32),
+    Get,
+    Panic,
+}
+
+/// Actor with an internal state that can me modified.
+struct InternalState {
+    counter: Mutex<u32>
+}
+
+impl Actor<InternalStateMessage> for InternalState {
+    fn receive<Args: Message>(&self, message: InternalStateMessage, _context: ActorCell<Args, InternalStateMessage, InternalState>) {
+        match message {
+            InternalStateMessage::Get => println!("internal state: {}", *self.counter.lock().unwrap()),
+            InternalStateMessage::Set(num) => *self.counter.lock().unwrap() = num,
+            InternalStateMessage::Panic => panic!("actor panicked"),
+        }
+    }
+}
+
+impl InternalState {
+    fn new(count: u32) -> InternalState {
+        InternalState { counter: Mutex::new(count) }
+    }
+}
+
+fn main() {
+    let actor_system = ActorSystem::new("test".to_owned());
+    actor_system.spawn_threads(1);
+
+    let restarted_props = Props::new(Arc::new(InternalState::new), 3);
+    let restarted_actor_ref_1 = actor_system.actor_of(restarted_props.clone());
+    let restarted_actor_ref_2 = actor_system.actor_of(restarted_props.clone());
+
+    restarted_actor_ref_1.tell_to(restarted_actor_ref_2.clone(), InternalStateMessage::Get);
+    restarted_actor_ref_1.tell_to(restarted_actor_ref_2.clone(), InternalStateMessage::Set(7));
+    restarted_actor_ref_1.tell_to(restarted_actor_ref_2.clone(), InternalStateMessage::Get);
+    restarted_actor_ref_1.tell_to(restarted_actor_ref_2.clone(), InternalStateMessage::Panic);
+    // TODO(gamazeps): Having to wait here is a bug in the library.
+    // Indeed, there is currently no way to check that an actor has been restarted if it failed.
+    // Here we explicitly wait for it to be restarted.
+    std::thread::sleep(Duration::from_millis(1));
+    restarted_actor_ref_1.tell_to(restarted_actor_ref_2.clone(), InternalStateMessage::Get);
+
+    std::thread::sleep(Duration::from_millis(1));
+    actor_system.terminate_threads(1);
+}
