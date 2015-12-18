7f8c23d2d770d781217bc470acd512af7ca959e8
gamazeps <gamaz3ps@gmail.com>
2015-12-18 09:08:32 +0100

    Add a test for the ask methods.

--- test/test.rs
+++ test/test.rs
@@ -1,4 +1,7 @@
+extern crate eventual;
 extern crate robots;
+
+use eventual::Async;
 
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{channel, Sender};
@@ -11,21 +14,36 @@ enum Res {
     Err
 }
 
+#[derive(Copy, Clone)]
+enum InternalStateMessage {
+    Set(u32),
+    Get,
+    Panic,
+}
+
 struct InternalState {
     last: Mutex<u32>,
     sender: Arc<Mutex<Sender<Res>>>,
 }
 
-impl Actor<u32> for InternalState {
-    fn receive<Args: Arguments>(&self, message: u32, _context: ActorCell<Args, u32, InternalState>) {
-        let mut last = self.last.lock().unwrap();
-        if message <= *last {
-            let _ = self.sender.lock().unwrap().send(Res::Err);
-        } else {
-            *last = message;
-        }
-        if *last == 1000 {
-            let _ = self.sender.lock().unwrap().send(Res::Ok);
+impl Actor<InternalStateMessage> for InternalState {
+    fn receive<Args: Arguments>(&self, message: InternalStateMessage, context: ActorCell<Args, InternalStateMessage, InternalState>) {
+        match message {
+            InternalStateMessage::Get => context.tell(context.sender(), *self.last.lock().unwrap()),
+            InternalStateMessage::Set(message) => {
+                // Here mixing the test actir for the two tests might seem a bit weird,
+                // but we would get two very similar actors otherwise.
+                let mut last = self.last.lock().unwrap();
+                if message <= *last {
+                    let _ = self.sender.lock().unwrap().send(Res::Err);
+                } else {
+                    *last = message;
+                }
+                if *last == 1000 {
+                    let _ = self.sender.lock().unwrap().send(Res::Ok);
+                }
+            }
+            InternalStateMessage::Panic => panic!(""),
         }
     }
 }
@@ -52,11 +70,34 @@ fn read_messages_in_order() {
     let actor_ref_2 = actor_system.actor_of(props.clone(), "receiver".to_owned());
 
     for i in 1..1001 {
-        actor_ref_1.tell_to(actor_ref_2.clone(), i as u32);
+        actor_ref_1.tell_to(actor_ref_2.clone(), InternalStateMessage::Set(i as u32));
     }
 
     let res = rx.recv();
     assert_eq!(Ok(Res::Ok), res);
+
+    actor_system.shutdown();
+}
+
+#[test]
+fn recover_from_panic() {
+    let actor_system = ActorSystem::new("test".to_owned());
+    actor_system.spawn_threads(1);
+
+    let (tx, _rx) = channel();
+    let tx = Arc::new(Mutex::new(tx));
+
+    let props = Props::new(Arc::new(InternalState::new), tx);
+    let requester = actor_system.actor_of(props.clone(), "sender".to_owned());
+    let answerer = actor_system.actor_of(props.clone(), "receiver".to_owned());
+
+    requester.tell_to(answerer.clone(), InternalStateMessage::Set(10));
+    let res: u32 = requester.ask_to::<InternalStateMessage, u32, ()>(answerer.clone(), InternalStateMessage::Get).and_then(|x| Ok(x)).await().unwrap();
+    assert_eq!(10u32, res);
+
+    requester.tell_to(answerer.clone(), InternalStateMessage::Panic);
+    let res: u32 = requester.ask_to::<InternalStateMessage, u32, ()>(answerer.clone(), InternalStateMessage::Get).and_then(|x| Ok(x)).await().unwrap();
+    assert_eq!(0u32, res);
 
     actor_system.shutdown();
 }
